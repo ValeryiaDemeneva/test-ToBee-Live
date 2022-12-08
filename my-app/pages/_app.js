118accccc0e542f0fca7ebfb2f7ce7433cd3ef4e
artsiom.statuta <v.dddemeneva@gmail.com>
2022-12-08 14:48:59 +0100

    feat: add basic sructure of app

---
+++ my-app/pages/_app.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Input, List } from "../components";
+import "../styles/globals.css";
+
+function App() {
+  const [list, setList] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <div>
+    
+    </div>
+  );
+}
+
+export default App;
